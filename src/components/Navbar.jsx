afc9b2c83f3610f5fd5014866e29843a5632cbae
Anshika Pathak <anshikapathak25@navgurukul.org>
2026-07-20 15:28:44 +0530

    Implement routing and layout structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <nav>
-      <h2>Navbar</h2>
+    <nav
+      style={{
+        display: "flex",
+        gap: "20px",
+        padding: "20px",
+        justifyContent: "center",
+        borderBottom: "1px solid #ccc",
+      }}
+    >
+      <Link to="/">Home</Link>
+
+      <Link to="/products">Products</Link>
+
+      <Link to="/cart">Cart</Link>
+
+      <Link to="/login">Login</Link>
+
+      <Link to="/admin">Admin</Link>
     </nav>
   );
 };
